71b4228ea852fdb5454a2505f30831a16f950d71
Szymon Pyżalski <szymon@pythonista.net>
2013-05-02 11:52:15 +0200

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('entry_points.ini') as f:
 
 setup(
     name = 'Anthrax',
-    version = '0.0.5',
+    version = '0.0.6',
     author = 'Szymon Pyżalski',
     author_email = 'zefciu <szymon@pythonista.net>',
     description = 'Form generation lib to end all form generation lib',
